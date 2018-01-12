1a382ef56687cca170f851edfa2e759abde759c2
Bruno Machado Brandão <brunin5010@gmail.com>
2018-01-12 18:49:41 -0200

    Adicionando thrust.d.ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,9 @@ module.exports = {
   "globals": {
     "document": false,
     "navigator": false,
-    "window": false
+    "window": false,
+    "print": true,
+    "console": true
   },
 
   "rules": {
---
+++ thrust.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @project Thrust
+ * @author Nery
+ */
+declare var rootPath: string;
+
+/**
+* Carrega um módulo Javascript disponibilizando no contexto somente as
+* vari&aacute;veis e funções que foram exportadas.
+* @param {string} modulePath O nome do arquivo (módulo) a ser importado.
+* @returns {Object}
+*/
+declare function require(modulePath: string): object;
+
+/**
+* Printa a informação passada no console
+* @param {Object} obj Informação a ser escrita no console.
+*/
+declare function print(obj: Object): void;
+
+/**
+* Printa a informação passada no console
+* @param {Object} obj Informação a ser escrita no console.
+*/
+declare function show(obj: Object): void;
